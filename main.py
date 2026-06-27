8dd5df99fb07ae769bb57e9b810e1f5e164eb5dd
Krishna Malhotra <krishnamalhotra483@gmail.com>
2026-06-27 21:22:44 +0530

    Gemini Integration

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import webbrowser
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from datetime import datetime
-from config import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
+from config import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET,GEMINI_API_KEY
 from websites import websites
- 
+from google import genai
 
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+ 
 
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
@@ -25,7 +28,6 @@ def speak(text):
      engine.setProperty('voice', voices[0].id)
      engine.say(text)
      engine.runAndWait()
-     print("hello")
      
 
 def browse(tab):
@@ -34,6 +36,7 @@ def browse(tab):
         webbrowser.open_new_tab(websites[tab])
       else:
           speak("Website doesnt exist")
+
 
 def song(songname):
     try:
@@ -52,14 +55,24 @@ def time():
     hour=datetime.now().strftime("%H")
     minutes=datetime.now().strftime("%M")
     speak(f"The current time is {hour} hours and {minutes} minutes")
-     
+
+
+def process_ai(text):
+    interaction = client.interactions.create(
+      model="gemini-3.5-flash",
+      system_instruction="You are a personal assistant named Krishna.Dont tell your name again and again Since i will give your output to a text to speech convert dont use asterix comma hash as it speaks in text to speech. Keep output short",
+      input=text
+)
+    speak(interaction.output_text)
+
+
+
 def date():
      todaydate=datetime.now().strftime("%d")
      day=datetime.now().strftime("%A")
      year=datetime.now().strftime("%Y")
      month=datetime.now().strftime("%B")
      speak(f"Today is {day} {todaydate} of {month} of year {year}")
-
      
 
 def process(command):
@@ -71,6 +84,8 @@ def process(command):
          time()
     elif("date" in command):
          date()
+    else:
+        process_ai(command)
 
 
 if __name__=="__main__":
@@ -90,7 +105,6 @@ if __name__=="__main__":
                  command=r.recognize_google(audio)
                  print(command)
                  process(command.lower())
-
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
